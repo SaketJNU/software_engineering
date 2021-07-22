ec206dd833fad938b7287f83e8b59e746ef4e455
SaketJNU <saket_au@rediffmail.com>
2021-07-22 11:17:52 +0530

    22-07-2021

---
+++ BCA_VB/fstProgram.py
@@ -0,0 +1,9 @@
+print("Hello World!")              # Simple Print
+
+print("{0}".format("Hello World"))            # String Formatter
+
+print("Sum of {0} and {1} is {2}.".format(2,3,5))
+
+a = 3;
+b = 6;
+print("Sum of {} and {} is {}.".format(a, b, a+b))
